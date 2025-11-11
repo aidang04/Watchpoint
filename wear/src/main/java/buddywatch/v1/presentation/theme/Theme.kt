37a2ea4bfc8fe0de515350986ec90bf5a57ce59e
Aidan <aidangowdy@gmail.com>
2025-11-11 21:26:42 +0000

    Initial commit

---
+++ wear/src/main/java/buddywatch/v1/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package buddywatch.v1.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun BuddywatchTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
